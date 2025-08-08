0d4f837cc825f847c9d076667869c51ad6ac365d
Ilyas Isse <ilyasisse574@gmail.com>
2025-08-08 06:54:50 -0400

    NEW COLLECTION HAS DYNAMIC DATA SHOWING

--- src/components/home/NewCollections.jsx
+++ src/components/home/NewCollections.jsx
@@ -1,33 +1,55 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NewCollections() {
+
+
+
+  const [newCollections, setnewCollections] = useState([])
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const info = await axios.get("https://remote-internship-api-production.up.railway.app/newCollections")
+      const newData = info.data.data
+      setnewCollections(newData)
+    }
+
+    fetchData()
+
+  }, [])
+
+
+
+
+
   return (
     <section id="new-collections">
       <div className="container">
         <div className="row">
           <h2 className="new-collections__title">New Collections</h2>
           <div className="new-collections__body">
-            {new Array(6).fill(0).map((_, index) => (
-              <div className="collection-column">
-                <Link to="/collection" key={index} className="collection">
+            {newCollections.map((NFT, index) => (
+              <div className="collection-column"  key={index}>
+                <Link to="/collection" className="collection">
                   <img
-                    src="https://i.seadn.io/gcs/files/a5414557ae405cb6233b4e2e4fa1d9e6.jpg?auto=format&dpr=1&w=1920"
+                    src={NFT.imageLink}
                     alt=""
                     className="collection__img"
                   />
                   <div className="collection__info">
-                    <h3 className="collection__name">Bored Ape Kennel Club</h3>
+                    <h3 className="collection__name">{NFT.title}</h3>
                     <div className="collection__stats">
                       <div className="collection__stat">
                         <span className="collection__stat__label">Floor</span>
-                        <span className="collection__stat__data">0.46 ETH</span>
+                        <span className="collection__stat__data">{(Math.ceil(NFT.floor * 100) / 100).toFixed(2)} ETH</span>
                       </div>
                       <div className="collection__stat">
                         <span className="collection__stat__label">
                           Total Volume
                         </span>
-                        <span className="collection__stat__data">281K ETH</span>
+                        <span className="collection__stat__data">{NFT.totalVolume} ETH</span>
                       </div>
                     </div>
                   </div>
