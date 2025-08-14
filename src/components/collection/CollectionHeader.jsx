e328a57d8a9064270c1304401aedb807efa15047
Ilyas Isse <ilyasisse574@gmail.com>
2025-08-13 21:50:43 -0400

    Collection Page is all Dynamic Data and is not hard coded and also it has a loading state when fetching data it looking great

--- src/components/collection/CollectionHeader.jsx
+++ src/components/collection/CollectionHeader.jsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Skeleton from "../ui/Skeleton";
+
 
 export default function CollectionHeader() {
+  const { id } = useParams()
+
+  const [CollectionHeader, setCollectionHeader] = useState({})
+
+  useEffect(() => {
+
+    async function FetchData() {
+      const info = await axios.get(`https://remote-internship-api-production.up.railway.app/collection/${id}`)
+      const newData = info.data.data
+      setCollectionHeader(newData)
+    }
+    FetchData()
+  }, [])
+
+  if (Object.keys(CollectionHeader).length === 0) {
+
+    return (<div id="collection-header">
+      <Skeleton width="100%" height="100%" />
+    </div>
+    )
+  }
+
   return (
     <header
       style={{
         backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.95), rgba(0, 0, 0, 0.2)), 
-        url('https://i.seadn.io/gcs/files/cbeed39f76506b4baf71005d7127d0df.png?auto=format&dpr=1&w=1920')`,
+        url('${CollectionHeader.imageLink}')`
       }}
       id="collection-header"
     >
@@ -14,18 +39,18 @@ export default function CollectionHeader() {
         <div className="collection-header__content">
           <div className="collection-header__left">
             <img
-              src="https://i.seadn.io/gcs/files/2d036c8c2bed042a1588622c3173677f.png?auto=format&dpr=1&w=256"
+              src={CollectionHeader.logo}
               alt=""
               className="collection-header__img"
             />
-            <div className="collection-header__name">Meebits</div>
-            <Link to={'/user'} className="collection-header__author">C352B5</Link>
+            <div className="collection-header__name">{CollectionHeader.title}</div>
+            <Link to={'/user'} className="collection-header__author">{CollectionHeader.creator}</Link>
           </div>
           <div className="collection-header__right">
             <div className="collection-header__columns">
               <div className="collection-header__column">
                 <span className="collection-header__column__data">
-                  <span className="semibold">181,714</span> ETH
+                  <span className="semibold">{CollectionHeader.totalVolume}</span> ETH
                 </span>
                 <span className="collection-header__column__label">
                   Total volume
@@ -33,7 +58,7 @@ export default function CollectionHeader() {
               </div>
               <div className="collection-header__column">
                 <span className="collection-header__column__data">
-                  <span className="semibold">0.55</span> ETH
+                  <span className="semibold">{CollectionHeader.floor}</span> ETH
                 </span>
                 <span className="collection-header__column__label">
                   Floor price
@@ -41,7 +66,7 @@ export default function CollectionHeader() {
               </div>
               <div className="collection-header__column">
                 <span className="collection-header__column__data">
-                  <span className="semibold">0.5154</span> ETH
+                  <span className="semibold">{CollectionHeader.bestOffer}</span> ETH
                 </span>
                 <span className="collection-header__column__label">
                   Best offer
@@ -49,13 +74,13 @@ export default function CollectionHeader() {
               </div>
               <div className="collection-header__column">
                 <span className="collection-header__column__data">
-                  <span className="semibold">1%</span>
+                  <span className="semibold">{CollectionHeader.listed}</span>
                 </span>
                 <span className="collection-header__column__label">Listed</span>
               </div>
               <div className="collection-header__column">
                 <span className="collection-header__column__data">
-                  <span className="semibold">6,452 (32%)</span>
+                  <span className="semibold">{CollectionHeader.owners}</span>
                 </span>
                 <span className="collection-header__column__label">
                   Owners (Unique)
--- src/components/collection/CollectionItems.jsx
+++ src/components/collection/CollectionItems.jsx
@@ -1,10 +1,87 @@
 import { faEye } from "@fortawesome/free-regular-svg-icons";
 import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Skeleton from "../ui/Skeleton";
 
 export default function CollectionItems() {
+
+  const {id} = useParams()
+
+  const[CollectionItem, setCollectionItem] = useState([])
+
+  const[amount , setAmount] = useState()
+
+  const[NFTamount , setNFTamount] = useState(12)
+
+  const [sort, setSort] = useState("");
+
+  useEffect(() => {
+    async function FetchData() {
+      const info = await axios.get(`https://remote-internship-api-production.up.railway.app/collection/${id}`)
+      const NewData = info.data.data.items
+      setCollectionItem(NewData)
+    }
+    FetchData()
+  },[])
+
+  function sortNFT(){
+    if(sort === "HIGH__TO__LOW"){
+      setCollectionItem(CollectionItem.slice().sort((a,b) => b.price - a.price))
+    }
+    if(sort === "LOW__TO__HIGH"){
+      setCollectionItem(CollectionItem.slice().sort((a,b) => a.price - b.price))
+    }
+  }
+
+  useEffect(() => {
+    sortNFT()
+  },[sort])
+
+  if(Object.keys(CollectionItem).length === 0 ){
+    return(
+      <div className="row collection-items__row">
+        <div className="collection-items__header">
+          <div className="collection-items__header__left">
+            <div className="collection-items__header__live">
+              <Skeleton width="52px" height="16px" borderRadius="4px"/>
+            </div>
+            <div className="collection-items__header__results">
+              <Skeleton width="72px" height="16px" borderRadius="4px"/>
+            </div>
+          </div>
+          <div className="collection-items__header__sort">
+            <Skeleton width="240px" height="48px" borderRadius="8px"/>
+          </div>
+        </div>
+        <div className="collection-items__body">
+          {[...Array(NFTamount)].map((_,i) => (
+            <div className="item-column">
+              <div className="item">
+                <figure className="item__img__wrapper">
+                  <Skeleton width="100%" height="100%"/>
+                </figure>
+                <div className="item__details">
+                  <span className="item__details__name">
+                    <Skeleton width="80px" height="16px" borderRadius="4px"/>
+                  </span>
+                  <span className="item__details__price">
+                    <Skeleton width="48px" height="16px" borderRadius="4px"/>
+                  </span>
+                  <span className="item__details__last-sale">
+                    <Skeleton width="120px" height="16px" borderRadius="4px"/>
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <section id="collection-items">
       <div className="row collection-items__row">
@@ -15,33 +92,36 @@ export default function CollectionItems() {
               Live
             </span>
             <span className="collection-items__header__results">
-              10 results
+              {CollectionItem?.length} results
             </span>
           </div>
-          <select className="collection-items__header__sort">
+          <select className="collection-items__header__sort"
+          value={sort}
+          onChange={(event) => setSort(event.target.value)}
+          >
             <option value="" default>
               Default
             </option>
-            <option value="">Price high to low</option>
-            <option value="">Price low to high</option>
+            <option value="HIGH__TO__LOW">Price high to low</option>
+            <option value="LOW__TO__HIGH">Price low to high</option>
           </select>
         </div>
         <div className="collection-items__body">
-          {new Array(8).fill(0).map((_, index) => (
+          {CollectionItem.slice(0,NFTamount).map((NFT, index) => (
             <div className="item-column">
               <Link to={"/item"} key={index} className="item">
                 <figure className="item__img__wrapper">
                   <img
-                    src="https://i.seadn.io/gcs/files/0a085499e0f3800321618af356c5d36b.png?auto=format&dpr=1&w=384"
+                    src={NFT.imageLink}
                     alt=""
                     className="item__img"
                   />
                 </figure>
                 <div className="item__details">
-                  <span className="item__details__name">Meebit #0001</span>
-                  <span className="item__details__price">0.98 ETH</span>
+                <span className="item__details__name">{NFT.title}</span>
+                  <span className="item__details__price">{NFT.price} ETH</span>
                   <span className="item__details__last-sale">
-                    Last sale: 7.45 ETH
+                    Last sale: {NFT.lastSale} ETH
                   </span>
                 </div>
                 <div className="item__see-more">
@@ -55,7 +135,14 @@ export default function CollectionItems() {
           ))}
         </div>
       </div>
-      <button className="collection-page__button">Load more</button>
+      {NFTamount < CollectionItem.length&& (
+        <button
+         className="collection-page__button"
+         onClick={() => {
+          setNFTamount(prev => prev + 6)
+         }}
+         >Load more</button>
+      )}
     </section>
   );
 }
